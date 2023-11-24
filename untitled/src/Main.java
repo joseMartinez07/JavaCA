4612639b38f418750b96318e7a9be760e5b8fb53
joseMartinez07 <jdaniel.mcaceres@gmail.com>
2023-11-24 10:57:04 -0500

    24/11/2023 tipos de variables

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,8 +2,25 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 
+// estos programas son para recordar y estar practicando
+
 public class Main {
     public static void main(String[] args) {
+        // en pesareos por los tipos de datos
+        // variable sin inicializar
+        // los tipos primitivos que se usan para varible siempre empiezan en minuscula
+        // los tipos que empiezan en mayuscula son para poder tratat el tipo como objeto
+        int var1; // esta es una variables de tipo entero
+        short var2; // esta es una variable de tipo short
+        long var3; // esta es una variable de tipo long
+        byte var4; // esta es una variable de tipo Byte
+        // varibles con coma flotante
+        float var5; // esta es una variable de tipo flotante
+        double var6; // esta es una variabñe de tipo doble
+        // variables para cadenas y caracteres
+        char var7; // esta es una variable de tipo char para caracteres
+        String var8; // esta es una variable de tipo str para  cadena de caracteres
+        
         System.out.print("Hola mundo haciendo pruebas de git y java");
     }
 }
